2cc0e5cb7f92b6b5db4faf0b9d7f0d15d2e3bd83
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-08 18:59:47 +0100

    initialize correctly project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "@infusedui/core/dist/infusedui.style.css";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <>
+      <h1>TeuTeuTrainCo</h1>
+    </>
+  );
+};
+
+export default App;
