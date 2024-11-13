968f0e0fed796319d46d1771ba377d9c0e0a4e94
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-13 10:49:44 +0100

    Create Home.tsx

---
+++ www/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+const Home = () => {
+  return (
+    <div
+      className="teaui body"
+      style={{
+        background:
+          "url(https://images.pexels.com/photos/1793503/pexels-photo-1793503.jpeg",
+        backgroundSize: "contain",
+      }}
+    >
+      <h1>TeuTeuTrain Company</h1>
+    </div>
+  );
+};
+
+export default Home;
