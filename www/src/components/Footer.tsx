2b74a00021384c8470511467ad85b2bdd24c1746
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-11-13 10:49:08 +0100

    add base components

---
+++ www/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="teaui footer">
+      <div className="teaui format-page sf"></div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ www/src/components/Header.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="teaui header-root">
+      <div className="teaui header-container">
+        <Link to={"/"}>
+          <img src="/imgs/logo.svg" height={32} />
+        </Link>
+        <Nav />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+const Nav = () => {
+  return (
+    <nav className="teaui header-nav">
+      <ul>
+        <li>
+          <Link to={"/ttgv"}>TTGV</Link>
+        </li>
+        <li>
+          <Link to={"/ter"}>TER</Link>
+        </li>
+        <li>
+          <Link to={"/fret"}>FRET</Link>
+        </li>
+        <li>
+          <Link to={"/valeurs"}>Valeurs</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
