ff3fb2f118d7a26dde979b2e6eb84f5ff0f67bf3
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-12 18:48:14 +0100

    Create PartHeroHeader.tsx

---
+++ www/src/components/parts/PartHeroHeader.tsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+
+const PartHeroHeader = () => {
+  return (
+    <div
+      className="teaui grid format-page sxl radius-small shadow browser mt80"
+      style={{
+        background:
+          "url(https://images.pexels.com/photos/258444/pexels-photo-258444.jpeg",
+        backgroundSize: "100%",
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="teaui grid format-page sl sv-60 va-center">
+        <h1 className="teaui mb80">
+          Vous transporter,
+          <br />
+          notre métier.
+        </h1>
+
+        <div className="teaui grid tac">
+          <Link
+            to={""}
+            className="teaui cta format-icon-right level-primary sl"
+          >
+            <span>En savoir plus</span>
+            <i className="icon teaui-icon-arrow-right"></i>
+          </Link>
+          <Link
+            to={""}
+            className="teaui cta format-icon-right level-secondary sl"
+          >
+            <span>Faire carrière</span>
+            <i className="icon teaui-icon-arrow-right"></i>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PartHeroHeader;
