6beaac5a9407dc96eb1265acb87e743f8c76e6e1
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-13 17:05:11 +0100

    Update SectionGare.tsx

--- www/src/components/sections/SectionGare.tsx
+++ www/src/components/sections/SectionGare.tsx
@@ -1,41 +1,22 @@
+import { EmblaOptionsType } from "embla-carousel";
+import EmblaCarousel from "@part/PartEmblaCarousel";
+import "../../styles/bin/Embla.scss";
+
 const SectionGare = () => {
+  const OPTIONS: EmblaOptionsType = { align: "start", dragFree: true };
+  const SLIDES = [
+    { media: "./imgs/canvas/gare-est-1000.png" },
+    { media: "./imgs/canvas/gare-nord-1000.png" },
+    { media: "./imgs/canvas/gare-ouest-1000.png" },
+    { media: "./imgs/canvas/gare-sud-1000.png" },
+    { media: "./imgs/canvas/gare-nether.png" },
+    { media: "./imgs/canvas/gare-ouest-usine-fer.png" },
+  ];
+
   return (
     <div className="teaui grid format-page sl mt80">
       <h2>Nos gares</h2>
-      <div className="teaui grid tc-3">
-        <div className="teaui grid shape-square pa0">
-          <img
-            src="./imgs/canvas/gare-est-1000.png"
-            className="teaui media shape-round"
-            width={"100%"}
-            alt=""
-          />
-        </div>
-        <div className="teaui grid shape-square pa0">
-          <img
-            src="./imgs/canvas/gare-nord-1000.png"
-            className="teaui media shape-round"
-            width={"100%"}
-            alt=""
-          />
-        </div>
-        <div className="teaui grid shape-square pa0">
-          <img
-            src="./imgs/canvas/gare-ouest-1000.png"
-            className="teaui media shape-round"
-            width={"100%"}
-            alt=""
-          />
-        </div>
-        <div className="teaui grid shape-square pa0">
-          <img
-            src="./imgs/canvas/gare-sud-1000.png"
-            className="teaui media shape-round"
-            width={"100%"}
-            alt=""
-          />
-        </div>
-      </div>
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
     </div>
   );
 };
