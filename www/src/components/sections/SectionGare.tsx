b333a6e3f50d5e897ea17b862b3436882d93172d
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-20 17:12:02 +0100

    Update SectionGare.tsx

--- www/src/components/sections/SectionGare.tsx
+++ www/src/components/sections/SectionGare.tsx
@@ -5,12 +5,32 @@ import "../../styles/bin/Embla.scss";
 const SectionGare = () => {
   const OPTIONS: EmblaOptionsType = { align: "start", dragFree: true };
   const SLIDES = [
-    { media: "./imgs/canvas/gare-est-1000.png" },
-    { media: "./imgs/canvas/gare-nord-1000.png" },
-    { media: "./imgs/canvas/gare-ouest-1000.png" },
-    { media: "./imgs/canvas/gare-sud-1000.png" },
-    { media: "./imgs/canvas/gare-nether.png" },
-    { media: "./imgs/canvas/gare-ouest-usine-fer.png" },
+    {
+      media: "./imgs/canvas/gare-est-1000.png",
+      link: "ailleurs-sur-loin",
+      label: "",
+    },
+    {
+      media: "./imgs/canvas/gare-nord-1000.png",
+      link: "soleil-en-vadrouille",
+      label: "",
+    },
+    {
+      media: "./imgs/canvas/gare-ouest-1000.png",
+      link: "bordures-les-finis",
+      label: "",
+    },
+    {
+      media: "./imgs/canvas/gare-sud-1000.png",
+      link: "froidville-en-soupir",
+      label: "",
+    },
+    { media: "./imgs/canvas/gare-nether.png", link: "nether", label: "" },
+    {
+      media: "./imgs/canvas/gare-ouest-usine-fer.png",
+      link: "rouille-sur-acier",
+      label: "",
+    },
   ];
 
   return (
