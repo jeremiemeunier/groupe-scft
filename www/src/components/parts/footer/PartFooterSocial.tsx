901b26799b788922bc20fbd114bcf1f8c6a978b8
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-25 14:17:09 +0100

    Update PartFooterSocial.tsx

--- www/src/components/parts/footer/PartFooterSocial.tsx
+++ www/src/components/parts/footer/PartFooterSocial.tsx
@@ -1,11 +1,6 @@
-import { Link } from "react-router-dom";
-
 const PartFooterSocial = () => {
   return (
-    <div className="teaui footer-column gwc-2">
-      <Link to={"/"}>
-        <img src="/imgs/logo.svg" height={32} />
-      </Link>
+    <div className="teaui footer-column">
       <p>Vous transporter, notre métier.</p>
       <p className="teaui text-settings font-size-12 mt40">
         © {new Date().getFullYear()} — TeuTeuTrain Company
