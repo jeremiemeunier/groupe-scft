9e03e0cdf4052871ce81c20d1c5f4c02c76a7621
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-25 17:43:43 +0100

    Update PartFooterLogo.tsx

--- www/src/components/parts/footer/PartFooterLogo.tsx
+++ www/src/components/parts/footer/PartFooterLogo.tsx
@@ -8,6 +8,11 @@ const PartFooterLogo = () => {
           <img src="/imgs/ttt-groupe-logo.svg" height={64} />
         </Link>
       </div>
+      <div className="teaui grid tac">
+        <Link to={"/sgp"}>
+          <img src="/imgs/societe-grand-projet-color-black.svg" height={56} />
+        </Link>
+      </div>
     </div>
   );
 };
