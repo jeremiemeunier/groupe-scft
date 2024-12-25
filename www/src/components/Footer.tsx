ae51daacf6bc42f553dc82a3b4efcae0ed028def
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-25 17:10:21 +0100

    Update Footer.tsx

--- www/src/components/Footer.tsx
+++ www/src/components/Footer.tsx
@@ -1,12 +1,20 @@
 import PartFooterLogo from "@part/footer/PartFooterLogo";
-import PartFooterSocial from "@part/footer/PartFooterSocial";
+import PartFooterLinks from "@part/footer/PartFooterLinks";
+import PartFooterFirms from "@part/footer/PartFooterFirms";
 
 const Footer = () => {
   return (
     <footer className="teaui footer mt120">
-      <div className="teaui grid format-page sl tc-5">
+      <div className="teaui grid format-page sl tc-4">
         <PartFooterLogo />
-        <PartFooterSocial />
+        <PartFooterLinks />
+        <PartFooterFirms />
+      </div>
+      <div className="teaui grid format-page sl pt40">
+        <p>
+          Vous transporter, notre métier — © {new Date().getFullYear()} —
+          TeuTeuTrain Company
+        </p>
       </div>
     </footer>
   );
