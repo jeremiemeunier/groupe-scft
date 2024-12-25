2157c055b5a09a8ad4d2b772b3e7d1364dd9faff
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-25 14:17:05 +0100

    Update Footer.tsx

--- www/src/components/Footer.tsx
+++ www/src/components/Footer.tsx
@@ -1,9 +1,11 @@
+import PartFooterLogo from "@part/footer/PartFooterLogo";
 import PartFooterSocial from "@part/footer/PartFooterSocial";
 
 const Footer = () => {
   return (
     <footer className="teaui footer mt120">
       <div className="teaui grid format-page sl tc-5">
+        <PartFooterLogo />
         <PartFooterSocial />
       </div>
     </footer>
