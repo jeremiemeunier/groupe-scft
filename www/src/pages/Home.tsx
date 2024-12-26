cd41108381cd32f4e55c3fa5e8d7b403253c9cb1
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-26 16:58:32 +0100

    Update Home.tsx

--- www/src/pages/Home.tsx
+++ www/src/pages/Home.tsx
@@ -1,7 +1,11 @@
 import PartHeroHeader from "@part/PartHeroHeader";
 import SectionGare from "@section/SectionGare";
+import { useEffect } from "react";
 
 const Home = () => {
+  useEffect(() => {
+    document.title = "Vous transporter, notre métier — TeuTeuTrain Company";
+  }, []);
   return (
     <div>
       <PartHeroHeader />
