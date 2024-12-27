f534c7c583de508f7ac2183bd5d7fb783ff47377
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-27 19:35:21 +0100

    Update PartHeroHeader.tsx

--- www/src/components/parts/PartHeroHeader.tsx
+++ www/src/components/parts/PartHeroHeader.tsx
@@ -2,14 +2,19 @@ import { Link } from "react-router-dom";
 
 const PartHeroHeader = () => {
   return (
-    <div className="teaui grid format-page sf tc-3">
+    <div className="teaui grid format-page sf tc-2">
       <div className="teaui grid bg-ocre radius-small mt80">
-        <div className="teaui grid grid-row-fr sv-70 rc-1 pa80">
-          <h1 className="teaui mb80">
-            Vous transporter,
-            <br />
-            notre métier.
-          </h1>
+        <div className="teaui grid sv-70 rc-1 pa80">
+          <p className="teaui text fs-80 fw-900">
+            Vous transporter, notre métier.
+          </p>
+          <p>
+            Vous avez toujours voulu travailler dans une entreprise où les rails
+            ne mènent pas toujours là où on pense ? À la TTT, on ne cherche pas
+            la perfection, on cherche des gens prêts à embarquer dans une
+            aventure unique. Posez des questions, perdez vos outils, mais
+            surtout : faites avancer nos trains (quand ils démarrent) !
+          </p>
 
           <div className="teaui grid tac va-end">
             <Link
@@ -22,37 +27,25 @@ const PartHeroHeader = () => {
           </div>
         </div>
       </div>
-      <div className="teaui grid bg-foret radius-small mt80">
-        <div className="teaui grid grid-row-fr sv-70 rc-1 pa80">
-          <h1 className="teaui mb80">
-            Vous transporter,
+      <div className="teaui grid bg-cobalt-reverse radius-small mt80">
+        <div className="teaui grid sv-70 rc-1 pa80">
+          <p className="teaui text fs-80 fw-900">
+            Pour des projets
             <br />
-            notre métier.
-          </h1>
+            (trop) grands
+          </p>
+          <p>
+            Vous rêvez de travailler sur des chantiers gigantesques où chaque
+            jour est une nouvelle énigme ? À la Société des Grands Projets, on
+            ne cherche pas des experts, on cherche des Teuteus motivés ! Posez
+            des rails, creusez des tunnels, ou essayez juste de retrouver les
+            plans… ici, tout est possible (surtout l’improbable).
+          </p>
 
           <div className="teaui grid tac va-end">
             <Link
               to={""}
-              className="teaui cta format-icon-right level-primary color-foret sl"
-            >
-              <span>Faire carrière</span>
-              <i className="icon teaui-icon-arrow-right"></i>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div className="teaui grid bg-ceruleen radius-small mt80">
-        <div className="teaui grid grid-row-fr sv-70 pa80">
-          <h1 className="teaui mb80">
-            Vous transporter,
-            <br />
-            notre métier.
-          </h1>
-
-          <div className="teaui grid tac va-end">
-            <Link
-              to={""}
-              className="teaui cta format-icon-right level-primary color-ceruleen sl"
+              className="teaui cta format-icon-right level-primary color-cobalt-reverse sl"
             >
               <span>Faire carrière</span>
               <i className="icon teaui-icon-arrow-right"></i>
