d21af903156fc3ed073294a1195c46ff2c1a4e77
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-27 21:38:44 +0100

    Update PartFooterLogo.tsx

--- www/src/components/parts/footer/PartFooterLogo.tsx
+++ www/src/components/parts/footer/PartFooterLogo.tsx
@@ -5,12 +5,12 @@ const PartFooterLogo = () => {
     <div className="teaui footer-column">
       <div className="teaui grid tac">
         <Link to={"/"}>
-          <img src="/imgs/ttt-groupe-logo.svg" height={56} />
+          <img src="/imgs/ttt-groupe-logo.png" height={56} />
         </Link>
       </div>
       <div className="teaui grid tac">
         <Link to={"/societes/societe-grands-projets"}>
-          <img src="/imgs/societe-grand-projet-color-black.svg" height={56} />
+          <img src="/imgs/societe-grand-projet-color-black.png" height={48} />
         </Link>
       </div>
     </div>
