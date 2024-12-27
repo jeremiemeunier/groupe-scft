ccf79dc99944b668a09daf38023e33339818a399
Jérémie Meunier <jeremie.meunier74@gmail.com>
2024-12-27 19:35:30 +0100

    Create SocietyLargeProject.tsx

---
+++ www/src/pages/SocietyLargeProject.tsx
@@ -0,0 +1,96 @@
+import { Link } from "react-router-dom";
+
+const PageSocietyLargeProject = () => {
+  return (
+    <div>
+      <div className="teaui grid bg-dark-cobalt-reverse va-center sv-80">
+        <div className="teaui grid format-page sl">
+          <p className="teaui text fs-80 fw-900">
+            Pour des projets
+            <br />
+            (trop) grands
+          </p>
+          <div className="teaui grid tc-2">
+            <p className="teaui mt80">
+              Ponts audacieux, tunnels interminables, et lignes de train
+              mythiques : aucun défi n’est trop grand pour cette équipe
+              d’experts… ou presque.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="teaui grid format-page pa64">
+        <h2>Notre mission</h2>
+        <p>
+          La SGP a pour ambition de connecter, transformer et magnifier
+          Teuteuland grâce à des infrastructures modernes (ou du moins
+          fonctionnelles). Avec un savoir-faire unique et une passion pour les
+          défis techniques improbables, la SGP ne recule devant rien, quitte à
+          prendre quelques détours.
+        </p>
+        <p>
+          La SGP, c’est l’assurance que les plus grandes idées de Teuteuland
+          deviennent réalité, même si parfois, il faut un peu de créativité pour
+          arriver au bout !
+        </p>
+      </div>
+
+      <div className="teaui grid format-page sl pa64">
+        <h2>Nos réalisations</h2>
+
+        <div className="teaui grid tc-2">
+          <Link
+            to={"/societe/societe-grands-projets/chantier/spawn-end-1h20"}
+            className="teaui card background color shade-nude pa32"
+          >
+            <h4 className="teaui pa0 ms-pa0 xs-pa0 ma0 ms-ma0 ff-title xs-ma0 text fs-56">
+              Rejoindre l'end en seulement 1h20
+            </h4>
+            <p>
+              Comment sommes nous en train de réaliser le plus grand chantier
+              ferroviaire de toutes la TeuTeuLandie en créant une liaison
+              directe entre le Grand Centre du Monde et l'End ?
+            </p>
+          </Link>
+          <Link
+            to={"/societe/societe-grands-projets/chantier/prolongement-nord"}
+            className="teaui card background color shade-ceruleen pa32"
+          >
+            <h4 className="teaui pa0 ms-pa0 xs-pa0 ma0 ms-ma0 ff-title xs-ma0 text fs-56">
+              Prolongement de la voie du Nord
+            </h4>
+            <p>
+              Parce qu’on a remarqué qu’il y avait encore de la place pour poser
+              des rails là-bas ! Avec cette extension, les Teuteus du Nord
+              pourront enfin dire : "On n’est plus perdus, on a une gare !" Un
+              projet ambitieux pour rapprocher les gens, même si ça rallonge un
+              peu le planning des trains.
+            </p>
+          </Link>
+        </div>
+      </div>
+
+      <div className="teaui grid format-page sl pa80 radius-small bg-dark-cobalt-reverse">
+        <h3>La SCP recrute !</h3>
+        <p>
+          Vous rêvez de travailler sur des chantiers gigantesques où chaque jour
+          est une nouvelle énigme ? À la Société des Grands Projets, on ne
+          cherche pas des experts, on cherche des Teuteus motivés ! Posez des
+          rails, creusez des tunnels, ou essayez juste de retrouver les plans…
+          ici, tout est possible (surtout l’improbable).
+        </p>
+
+        <Link
+          to={""}
+          className="teaui cta format-icon-right level-primary color-cobalt-reverse sl"
+        >
+          <span>Faire carrière</span>
+          <i className="icon teaui-icon-arrow-right"></i>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PageSocietyLargeProject;
