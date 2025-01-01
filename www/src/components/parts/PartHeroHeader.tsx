d9b761307d227beccef7a317beeaf3d975f669d4
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-01 09:25:39 +0100

    Update PartHeroHeader.tsx

--- www/src/components/parts/PartHeroHeader.tsx
+++ www/src/components/parts/PartHeroHeader.tsx
@@ -44,7 +44,7 @@ const PartHeroHeader = () => {
 
           <div className="teaui grid tac va-end">
             <Link
-              to={"/societe/societe-grands-projets/emplois"}
+              to={"/societes/societe-grands-projets/emplois"}
               className="teaui cta format-icon-right level-primary color-cobalt-reverse sl"
             >
               <span>Faire carrière</span>
