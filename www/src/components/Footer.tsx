c0a12784a4c6b0ed0b064fc2250958f60ffc531e
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-01 17:47:23 +0100

    Update Footer.tsx

--- www/src/components/Footer.tsx
+++ www/src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import PartFooterLogo from "@part/footer/PartFooterLogo";
 import PartFooterLinks from "@part/footer/PartFooterLinks";
 import PartFooterFirms from "@part/footer/PartFooterFirms";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -11,6 +12,15 @@ const Footer = () => {
         <PartFooterFirms />
       </div>
       <div className="teaui grid format-page tc-4 sl pt40">
+        <Link
+          to="https://bff.ecoindex.fr/redirect/?url=https://teuteutrainco.jeremiemeunier.fr"
+          target="_blank"
+        >
+          <img
+            src="https://bff.ecoindex.fr/badge/?theme=light&url=https://teuteutrainco.jeremiemeunier.fr"
+            alt="Ecoindex Badge"
+          />
+        </Link>
         <div className="teaui grid gwc-3 gsc-2">
           <p className="teaui text label">
             Vous transporter, notre métier — © {new Date().getFullYear()} —
