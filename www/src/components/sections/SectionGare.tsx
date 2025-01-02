48c78ba89c1bf7a64183c9fcdd9f325afd53ad17
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-02 20:07:30 +0100

    Update SectionGare.tsx

--- www/src/components/sections/SectionGare.tsx
+++ www/src/components/sections/SectionGare.tsx
@@ -6,32 +6,32 @@ const SectionGare = () => {
   const OPTIONS: EmblaOptionsType = { align: "start", dragFree: true };
   const SLIDES = [
     {
-      media: "./imgs/canvas/gare-est-1000-shape.png",
+      media: "./imgs/canvas/gare-est-1000-shape.webp",
       link: "ailleurs-sur-loin",
       label: "Ailleurs-sur-Loin",
     },
     {
-      media: "./imgs/canvas/gare-nord-1000-shape.png",
+      media: "./imgs/canvas/gare-nord-1000-shape.webp",
       link: "soleil-en-vadrouille",
       label: "Soleil-en-Vadrouille",
     },
     {
-      media: "./imgs/canvas/gare-ouest-1000-shape.png",
+      media: "./imgs/canvas/gare-ouest-1000-shape.webp",
       link: "bordures-les-finis",
       label: "Bordure-Lès-Finis",
     },
     {
-      media: "./imgs/canvas/gare-sud-1000-shape.png",
+      media: "./imgs/canvas/gare-sud-1000-shape.webp",
       link: "froidville-en-soupir",
       label: "Froidville-en-Soupir",
     },
     {
-      media: "./imgs/canvas/gare-nether-shape.png",
+      media: "./imgs/canvas/gare-nether-shape.webp",
       link: "nether",
       label: "Nether",
     },
     {
-      media: "./imgs/canvas/gare-ouest-usine-fer-shape.png",
+      media: "./imgs/canvas/gare-ouest-usine-fer-shape.webp",
       link: "rouille-sur-acier",
       label: "Rouille-sur-Acier",
     },
