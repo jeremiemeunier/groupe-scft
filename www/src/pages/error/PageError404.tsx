e8e8abff8e5d4cfad841975f6e855280b6fbaaa4
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-05 16:21:03 +0100

    Update PageError404.tsx

--- www/src/pages/error/PageError404.tsx
+++ www/src/pages/error/PageError404.tsx
@@ -11,17 +11,23 @@ const PageError404 = () => {
   }, []);
 
   return (
-    <div className="teaui grid format-page sv-100 va-center">
-      <h2 className="teaui text align-center">
-        Il semblerait que vous n’ayez pas pris la bonne direction...
-      </h2>
-      <p className="teaui text align-center">
-        Cette page est introuvable ou n’existe plus.
-      </p>
-      <div className="teaui grid ha-center">
-        <Link to={"/"} className="teaui cta level-primary sl">
-          Rejoindre l'accueil
-        </Link>
+    <div className="teaui grid bg-vert-eau sv-100 va-center">
+      <div className="teaui grid format-page ha-center">
+        <img src="/imgs/assets/assets_eyes.webp" height={"120px"} />
+        <h2 className="teaui text align-center">
+          Il semblerait que vous n’ayez pas pris la bonne direction...
+        </h2>
+        <p className="teaui text align-center">
+          Cette page est introuvable ou n’existe plus.
+        </p>
+        <div className="teaui grid ha-center">
+          <Link
+            to={"/"}
+            className="teaui cta shade color-vert-eau level-primary sl"
+          >
+            Rejoindre l'accueil
+          </Link>
+        </div>
       </div>
     </div>
   );
