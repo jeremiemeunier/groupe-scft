9977a57314da1f2eced3921de4830fc7b6123321
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-06 19:13:06 +0100

    add header

---
+++ website/src/components/Header.tsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className={"teaui header-root"}>
+      <div className="teaui header-container">
+        <div className="teaui grid tac va-center">
+          <Logo />
+        </div>
+        <div className="teaui grid ha-end">
+          <Nav />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+const Logo = () => {
+  return (
+    <div className="teaui app-logo">
+      <Link href={"/"}>
+        <img src="/imgs/ttt-groupe-logo.webp" height={"40px"} />
+      </Link>
+    </div>
+  );
+};
+
+const Nav = () => {
+  return (
+    <nav className={"teaui header-nav"}>
+      <ul>
+        <li>
+          <Link href={"/trains"}>Trains</Link>
+        </li>
+        <li>
+          <Link href={"/valeurs"}>Valeurs</Link>
+        </li>
+        <li>
+          <Link href={"/societes/gare-connexions"}>Nos gares</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
