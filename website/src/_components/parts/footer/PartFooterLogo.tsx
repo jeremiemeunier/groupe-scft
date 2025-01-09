00cd6324c662371a3f973a926561d74e9a64c772
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-09 18:34:33 +0100

    Update PartFooterLogo.tsx

--- website/src/_components/parts/footer/PartFooterLogo.tsx
+++ website/src/_components/parts/footer/PartFooterLogo.tsx
@@ -1,11 +1,24 @@
+"use client";
+
+import { ThemeContext } from "@/_context/ThemeContext";
 import Link from "next/link";
+import { useContext } from "react";
 
 const PartFooterLogo = () => {
+  const { websiteDarkTheme } = useContext(ThemeContext);
+
   return (
     <div className="teaui footer-column">
       <div className="teaui grid ha-start tac">
         <Link href={"/"}>
-          <img src="/imgs/ttt-groupe-logo.webp" height={80} />
+          <img
+            src={
+              websiteDarkTheme
+                ? `/imgs/logo_groupe_white_4x.webp`
+                : `/imgs/logo_groupe_color_4x.webp`
+            }
+            height={80}
+          />
         </Link>
       </div>
     </div>
