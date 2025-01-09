b13193e72e1ca8267bf271baf59d58d55a2920b0
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-09 19:37:46 +0100

    Update layout.tsx

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -5,9 +5,9 @@ import Footer from "@/_components/Footer";
 import ThemeProvider from "@/_context/ThemeContext";
 
 export const metadata: Metadata = {
-  title: "Vous transportez, notre métier — TeuTeuTrain Company",
+  title: "Accueil — SCFT Groupe",
   description:
-    "La TeuTeuTrain Company est l'entreprise de transport, de gestion et de construction d'infrastructure ferroviaire minecraftienne de prédilection de la Raie Publique de TeuTeuLand.",
+    "La SCFT est l'entreprise de transport, de gestion et de construction d'infrastructure ferroviaire minecraftienne de prédilection de la Raie Publique de TeuTeuLand.",
   icons: "/imgs/icon-colored.webp",
 };
 
