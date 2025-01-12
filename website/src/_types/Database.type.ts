ddb2877172420586f24b84f21583ea0ead20aee2
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-12 12:00:23 +0100

    Create Database.type.ts

---
+++ website/src/_types/Database.type.ts
@@ -0,0 +1,26 @@
+export interface ArticleInterface {
+  id: string;
+  created_at: string;
+  content: ArticleBlockCardInterface[];
+  title: string;
+  enc_title: string;
+  resume: string;
+  author: string;
+  color?: string;
+}
+
+export interface ArticleBlockInterface {
+  class?: string[];
+  elements: ArticleBlockElementInterface[];
+}
+
+export interface ArticleBlockElementInterface {
+  type: string;
+  content: string | ArticleBlockCardInterface[];
+  class?: string[];
+}
+
+export interface ArticleBlockCardInterface {
+  media: string;
+  content: ArticleBlockElementInterface[];
+}
