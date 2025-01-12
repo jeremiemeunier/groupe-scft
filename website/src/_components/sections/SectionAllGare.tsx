aa9dbfa51cc1a857e5bbdb7702af0783ca7c991e
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-12 12:00:16 +0100

    Update SectionAllGare.tsx

--- website/src/_components/sections/SectionAllGare.tsx
+++ website/src/_components/sections/SectionAllGare.tsx
@@ -7,19 +7,21 @@ const SectionAllGare = async () => {
 
   return (
     <div className="teaui grid bg-corail pa64">
-      <div className="teaui grid rgs-0 format-page sl">
+      <div className="teaui grid format-page sl">
         <h2>Toutes nos gares</h2>
 
-        {gare?.map((doc, key) => (
-          <Link
-            key={key}
-            className="teaui color shade-corail text-link"
-            href={`/societes/gare-connexions/${doc.enc_name}`}
-          >
-            <span className="teaui text fs-24 fw-600">{doc.name}</span>
-            <i className="icon teaui-icon-arrow-right"></i>
-          </Link>
-        ))}
+        <div className="teaui grid tc-2 cgs-24 rgs-0">
+          {gare?.map((doc, key) => (
+            <Link
+              key={key}
+              className="teaui color shade-corail text-link"
+              href={`/societes/gare-connexions/${doc.enc_name}`}
+            >
+              <span className="teaui text fs-24 fw-600">{doc.name}</span>
+              <i className="icon teaui-icon-arrow-right"></i>
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
