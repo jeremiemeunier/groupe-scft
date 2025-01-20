f98fdc06466a09c404a7f1b42d9d2c2709afabf0
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-20 19:17:22 +0100

    Update PartFooterLogo.tsx

--- website/src/_components/parts/footer/PartFooterLogo.tsx
+++ website/src/_components/parts/footer/PartFooterLogo.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ThemeContext } from "@/_context/ThemeContext";
+import Image from "next/image";
 import Link from "next/link";
 import { useContext } from "react";
 
@@ -11,13 +12,15 @@ const PartFooterLogo = () => {
     <div className="teaui footer-column">
       <div className="teaui grid ha-start tac">
         <Link href={"/"}>
-          <img
+          <Image
             src={
               websiteDarkTheme
                 ? `/imgs/logo_groupe_white_4x.webp`
                 : `/imgs/logo_groupe_color_4x.webp`
             }
             height={80}
+            width={117.51}
+            alt="Logo SCFT"
           />
         </Link>
       </div>
