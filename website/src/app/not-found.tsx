1a02856cacc7546dc21551f7e814b3967c3cf217
Jérémie Meunier <jeremie.meunier74@gmail.com>
2025-01-20 19:17:35 +0100

    Update not-found.tsx

--- website/src/app/not-found.tsx
+++ website/src/app/not-found.tsx
@@ -2,6 +2,7 @@
 // import { useContext, useEffect } from "react";
 
 import { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
@@ -19,19 +20,19 @@ const NotFound = () => {
   return (
     <div className="teaui grid bg-vert-eau sv-100 va-center">
       <div className="teaui grid format-page ha-center">
-        <img src="/imgs/assets/assets_eyes.webp" height={"120px"} />
+        <Image src="/imgs/assets/assets_eyes.webp" height={"120"} alt="" />
         <h2 className="teaui text align-center">
-          Il semblerait que vous n’ayez pas pris la bonne direction...
+          Il semblerait que vous n&apos;ayez pas pris la bonne direction...
         </h2>
         <p className="teaui text align-center">
-          Cette page est introuvable ou n’existe plus.
+          Cette page est introuvable ou n&apos;existe plus.
         </p>
         <div className="teaui grid ha-center">
           <Link
             href={"/"}
             className="teaui cta shade color-vert-eau level-primary sl"
           >
-            Rejoindre l'accueil
+            Rejoindre l&apos;accueil
           </Link>
         </div>
       </div>
